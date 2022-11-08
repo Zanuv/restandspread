9afdda66faadc8fc9cbdfd1c3aa5f6cad3806f00
Zach <zanuv@outlook.com>
2022-11-07 21:14:10 -0600

    Second commit

--- rest-spread.js
+++ rest-spread.js
@@ -1,4 +1,6 @@
-// Given this function, refactor it to use the rest operator and arrow function
+// Given this function
+// Refactor it to use the rest operator and arrow function
+// Write an ES2015 Version
 
 function filterOutOdds() {
   var nums = Array.prototype.slice.call(arguments);
@@ -6,4 +8,20 @@ function filterOutOdds() {
     return num % 2 === 0;
   });
 }
-function test() {}
+
+// Write a function called findMin that accepts a variable number of arguments
+// and returns the smallest argument.
+// Make sure to do this using the rest and spread operator.
+/* 
+findMin(1,4,12,-3) // -3
+findMin(1,-1) // -1
+findMin(3,1) // 1 */
+
+function findMin() {}
+
+// Write a function called mergeObjects that acceots two objects
+// and returns a new object which contains all the keys and values
+// of the first object and second object.
+// mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+
+function mergeObjects() {}
