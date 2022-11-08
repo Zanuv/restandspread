f871eb97a7b15f96d685a041a35c387efeb8cc9d
Zach <zanuv@outlook.com>
2022-11-07 20:21:49 -0600

    first commit

---
+++ rest-spread.js
@@ -0,0 +1,8 @@
+// Given this function, refactor it to use the rest operator and arrow function
+
+function filterOutOdds() {
+  var nums = Array.prototype.slice.call(arguments);
+  return nums.filter(function (num) {
+    return num % 2 === 0;
+  });
+}
